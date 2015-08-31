1e4313a90d0e6682bc84ec78a333c00ba8cbfead
Austin Blackstone <austinblackstoneengineering@gmail.com>
2015-08-31 18:30:46 -0500

    updated readme and submitted working version of script

--- meme.py
+++ meme.py
@@ -9,14 +9,31 @@ from PIL import Image
 from PIL import ImageDraw
 from PIL import ImageFont
 
-cookie = Image.open(os.path.join(os.path.abspath("."),"resources\\fortunecookie.jpg"))
-# grab text from command line, `meme.py "example text"`
-textRaw = sys.argv[1]+", mbed"
-fontPath = os.path.join(os.path.abspath("."),"resources\\gilsansalt.TTF")
-gilsansalt = ImageFont.truetype(fontPath,20)
-textImage = Image.new('RGB',(300,70),(0xFF,0xFF,0xFF))
-draw = ImageDraw.Draw(textImage)
-draw.text((0,0),textRaw,fill = "black",font=gilsansalt)
 
-cookie.paste(textImage,(243,130))
-cookie.save(os.path.join(os.path.abspath("."),"\\output.jpg"))
+# function taken from http://python.developermemo.com/6086_7715119/
+def wrapText(draw, text,font, fill = "black"):
+	import textwrap
+	w,h = draw.size
+	lines = textwrap.wrap(text, width)
+	y_text = h
+	for line in lines:
+		width, height = font.getsize(line)
+		draw.text(((w - width)/2, y_text), line, font = font, fill = FOREGROUND)
+		y_text += height
+
+
+def main():
+	cookie = Image.open(os.path.join(os.path.abspath("."),"resources\\fortunecookie.jpg"))
+# grab text from command line, `meme.py "example text"`
+	textRaw = (sys.argv[1]+", mbed").decode('utf-8')
+	fontPath = os.path.join(os.path.abspath("."),"resources\\gilsansalt.TTF")
+	gilsansalt = ImageFont.truetype(fontPath,20)
+	textImage = Image.new('RGB',(300,70),(0xFF,0xFF,0xFF))
+	draw = ImageDraw.Draw(textImage)
+#	wrapText(draw = draw, text = textRaw, font = gilsansalt)
+	draw.text((0,0),textRaw,fill = "black",font=gilsansalt)
+	cookie.paste(textImage,(243,130))
+	cookie.save(os.path.join(os.path.abspath("."),"\\output.jpg"))
+
+
+main()
--- readme.md
+++ readme.md
@@ -6,3 +6,11 @@ A generator script that takes input text and then puts it into a fortune cookie
 
 ## What is it for
 shits 'n giggles
+
+## How to use it
+
+Run a command like so
+`./meme.py "some example text" `
+
+and get an output like so...
+![memeGenerator Output](https://github.com/BlackstoneEngineering/memeGenerator/blob/master/output.jpg)
