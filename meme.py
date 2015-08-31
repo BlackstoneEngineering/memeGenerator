ab4b16adcb0d0183bc89304bcc0d324e5b5bef75
Austin Blackstone <austinblackstoneengineering@gmail.com>
2015-08-31 14:05:24 -0500

    added font support for meme generator

--- meme.py
+++ meme.py
@@ -6,12 +6,15 @@
 import os
 from PIL import Image
 from PIL import ImageDraw
+from PIL import ImageFont
 
 cookie = Image.open(os.path.join(os.path.abspath("."),"resources\\fortunecookie.jpg"))
 textRaw = "Programattically awesome, mbed"
+fontPath = os.path.join(os.path.abspath("."),"resources\\gilsansalt.TTF")
+gilsansalt = ImageFont.truetype(fontPath,20)
 textImage = Image.new('RGB',(300,70),(0xFF,0xFF,0xFF))
 draw = ImageDraw.Draw(textImage)
-draw.text((50,50),textRaw,fill = "black")
+draw.text((0,0),textRaw,fill = "black",font=gilsansalt)
 
 cookie.paste(textImage,(243,130))
 cookie.save(os.path.join(os.path.abspath("."),"\\output.jpg"))
